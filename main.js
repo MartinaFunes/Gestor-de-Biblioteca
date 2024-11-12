88ed4f61524600c7e0a5473f3b046cbdd3f50c39
Martina Funes <martinafunez21@gmail.com>
2024-11-12 15:54:44 -0300

    agregar java

---
+++ main.js
@@ -0,0 +1,15 @@
+const biblioteca = []; //Array para almacenar libros
+
+function iniciarSimulador(){
+    let continuar = true;
+
+    while(continuar){
+        let opcion = prompt("Seleccione una opción: \n1.Agregar Libro \n2.Buscar Libro \n3.Editar Libros \n4.Salir");
+
+        switch (opcion){
+            case "1":
+                
+        }
+    }
+
+}
